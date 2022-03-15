a8f23f6523ed3030f84baab6c419b528c5160ad1
Lendl Peralta <janlendl@gmail.com>
2022-03-15 17:18:02 -0600

    Add typical feature. Add button contact me button (hire me) and download resume button

--- frontend/src/Components/Home/Profile.js
+++ frontend/src/Components/Home/Profile.js
@@ -1,3 +1,4 @@
+import Typical from 'react-typical';
 
 export default function Profile() {
   return (
@@ -6,14 +7,55 @@ export default function Profile() {
         <div className='profile-details'>
           <div className='colz'>
             <a href='https://www.linkedin.com/in/janlendlperalta/' target='_blank' rel="noopener noreferrer">
-              <i className="fa fa-linkedin"></i>
+              <i className='fa fa-linkedin'></i>
             </a>
             <a href='https://github.com/janlendl' target='_blank' rel="noopener noreferrer">
-              <i className="fa fa-github"></i>
+              <i className='fa fa-github'></i>
+            </a>
+          </div>
+          <div className='profile-details-name'>
+            <span className='primary-text'>
+              {" "}
+              Hello, I'M <span className='highlighted-text'>Lendl</span>
+            </span>
+          </div>
+          <div className='profile-details-role'>
+            <span className='profile-text'>
+              {" "}
+              <h1>
+                {" "}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev 🤗",
+                    1000,
+                    "Full Stack Developer 👨‍💻",
+                    1000,
+                    "React Developer ⚛️",
+                    1000,
+                    "PERN Stack Dev 💻",
+                    1000,
+                    "Javascript ES6 ☕️",
+                    1000,
+                  ]}
+                />
+              </h1>
+              <span className='profile-role-tagline'>
+                Enjoy building applications with front and back end operations.
+              </span>
+            </span>
+          </div>
+          <div className='profile-options'>
+            <button className='btn primary-btn'>
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href='Lendl_Peralta_Resume.pdf' download='Lendl Lendl_Peralta_Resume.pdf'>
+                  <button className='btn highlighted-btn'>Get Resume</button>
             </a>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
