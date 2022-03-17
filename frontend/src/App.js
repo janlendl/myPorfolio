bae6681501eba91ab31a9b2703783739cc0fd326
Lendl Peralta <janlendl@gmail.com>
2022-03-17 12:35:22 -0600

    Replace Profile and Footer to Home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import Profile from './Components/Home/Profile'
+import Home from './Components/Home/Home'
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Home />
     </div>
   );
 }
