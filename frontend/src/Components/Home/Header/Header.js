ffb0072f979235cd8c82d840de16f1898b998ced
Lendl Peralta <janlendl@gmail.com>
2022-05-20 16:05:44 -0600

    import Header.css and remove ';' beside {getHeaderOptions}

--- frontend/src/Components/Home/Header/Header.js
+++ frontend/src/Components/Home/Header/Header.js
@@ -3,6 +3,8 @@ import { TOTAL_SCREENS, GET_SCREEN_INDEX } from "../../../utilities/commonUtils"
 import ScrollService from "../../../utilities/ScrollService";
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./Header.css";
+
 
 
 
@@ -68,7 +70,7 @@ export default function Header() {
           <span>LENDL~</span>
         </div>
         <div className={(showHeaderOptions)? "header-options show-hamburger-options": "header-options"}>
-          {getHeaderOptions};
+          {getHeaderOptions}
         </div>
       </div>
     </div>
